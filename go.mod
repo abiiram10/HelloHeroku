df839e3c601ac37c1ae419567f1dfc2924efa25a
Abiram López Hernández <abiiram10@gmail.com>
2022-04-07 21:30:36 -0500

    Subir todo el show

---
+++ go.mod
@@ -0,0 +1,3 @@
+module abiiram10/helloheroku
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+	log.Println("Server starting at port " + port + "...")
+
+	http.HandleFunc("/hello", helloHeroku)
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func helloHeroku(Response http.ResponseWriter, Request *http.Request) {
+	defer Request.Body.Close()
+
+	RespuestaBytes, err_marshal := json.Marshal(map[string]interface{}{
+		"Resultado": "hello",
+	})
+
+	if err_marshal != nil {
+		Response.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	Response.Header().Set("Access-Control-Allow-Origin", "*")
+	Response.Header().Set("Content-Type", "application/json")
+	Response.WriteHeader(http.StatusOK)
+	Response.Write(RespuestaBytes)
+}
